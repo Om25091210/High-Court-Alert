ee859cfd54de05f42c31186695c75473a5dfe11c
Nikhil Verma <nikhilverma.nv345@gmail.com>
2023-01-16 22:41:01 +0530

    share button and card_layout update

--- app/src/main/java/in/aryomtech/cgalert/Fragments/Adapter/Excel_Adapter.java
+++ app/src/main/java/in/aryomtech/cgalert/Fragments/Adapter/Excel_Adapter.java
@@ -204,7 +204,7 @@ public class Excel_Adapter extends RecyclerView.Adapter<Excel_Adapter.ViewHolder
                 "Name - " + list.get(position).getF()+  "\n" +
                 "Crime No. - " + list.get(position).getH() +"/"+ list.get(position).getI()+  "\n" +
                 "Received - " + list.get(position).getJ() + "\n\n"
-                + "उपरोक्त मूल केश डायरी दिनाँक "+list.get(position).getL()+" तक बेल शाखा, कार्यालय महाधिवक्ता,उच्च न्यायालय छतीसगढ़ में  अनिवार्यतः जमा करें।";
+                + "उपरोक्त मूल केस डायरी दिनाँक "+list.get(position).getL()+" तक बेल शाखा, कार्यालय महाधिवक्ता,उच्च न्यायालय छतीसगढ़ में अनिवार्यतः जमा करें।";
 
         holder.share.setOnClickListener(v->{
             Intent shareIntent = new Intent(Intent.ACTION_SEND);
